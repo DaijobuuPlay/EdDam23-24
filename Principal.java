73bd7bc73b2916244e4b44c7da6e9c722cbf4cdb
dam23-25 <dam23-25@c16x23.iescierva.net>
2024-01-30 10:56:43 +0100

    Añadido hora actual

--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
+import java.util.LocalDate;
+
 public class Principal {
     public void main (String[] args){
-        System.out.println("Hola mundo");    
+        System.out.println("Hola mundo");
+        horaActual();
+    }
+
+    public void horaActual(){
+        LocalDateTime locaDate = LocalDateTime.now();
+        int hours  = locaDate.getHour();
+        int minutes = locaDate.getMinute();
+        int seconds = locaDate.getSecond();
+        System.out.println("Hora actual : " + hours  + ":"+ minutes +":"+seconds);     
     }
 }
